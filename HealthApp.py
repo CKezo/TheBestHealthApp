8cbc491751021f833c21e1173f2235e48857a715
GegeeTs <tsgegee1030@gmail.com>
2023-10-19 19:05:25 -0600

    calories sulprus

--- HealthApp.py
+++ HealthApp.py
@@ -2,4 +2,7 @@ weight = int(input("Your weight: "))
 height = int(input("Your height: "))
 age = int(input("Your age: "))
 
-print("hello world")
+formula = int(10 * weight + 6.25 * height - 5 * age + 5)
+calories = 400 + formula
+print("To gain weight, you need" ,  calories , "calories per day")
+
