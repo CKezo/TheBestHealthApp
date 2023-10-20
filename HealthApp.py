4679addafa03562a70de072d6b4adab812c178d0
CKezo <kenzlo@uwalumni.com>
2023-10-19 19:28:01 -0600

    end

--- HealthApp.py
+++ HealthApp.py
@@ -1,13 +1,20 @@
 weight = int(input("Your weight: "))
 height = int(input("Your height: "))
 age = int(input("Your age: "))
+numberChoice = int(input("Would you like to 1: lose weight, 2: maintain weight, or 3: gain weight?"))
 
-formula = int(10 * weight + 6.25 * height - 5 * age + 5)
-calories = 400 + formula
-print("To gain weight, you need" ,  calories , "calories per day")
+formula = (10 * weight + 6.25 * height - 5 * age + 5)
+gainCalories = 400 + formula
+#print("To gain weight, you need" ,  calories , "calories per day")
 
-minorDeficit = (10*weight) + (6.25*height) + (5*age) - 250
-majorDeficit = (10*weight) + (6.25*height) + (5*age) - 500
+minorDeficit = ((10*weight) + (6.25*height) - (5*age)) - 250
+majorDeficit = (10*weight) + (6.25*height) - (5*age) - 500
 
-maintain = (f"To maintain your current weight, you need to consume {10*weight + 6.25*height - 5*age + 5} calories")
-print(maintain)
+maintain = 10*weight + 6.25*height - 5*age + 5
+#print(maintain)
+if numberChoice == 1:
+    print(f"In order to lose weight, you should eat {minorDeficit} calories per day")
+elif numberChoice == 2:
+    print(f"In order to maintain weight, you should eat {maintain} calories per day")
+elif numberChoice == 3:
+    print(f"In order to gain weight, you should eat {gainCalories} calories per day")
