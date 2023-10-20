65a1e0918356e2c36b8da95a67c621df7abb6d56
CKezo <kenzlo@uwalumni.com>
2023-10-19 19:00:15 -0600

    Here we go

--- HealthApp.py
+++ HealthApp.py
@@ -2,4 +2,7 @@ weight = int(input("Your weight: "))
 height = int(input("Your height: "))
 age = int(input("Your age: "))
 
-print("hello world")
+minorDeficit = (10*weight) + (6.25*height) + (5*age) - 250
+majorDeficit = (10*weight) + (6.25*height) + (5*age) - 500
+
+print("Would you like to lose weight, maintain weight, or gain weight?")
