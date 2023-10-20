817080e70ca3a72aa2056f0e53639a94fa89b718
Mustafo Sobirjonov <mustafo@Mustafos-MacBook-Pro.local>
2023-10-19 18:59:12 -0600

    maintain weight

--- HealthApp.py
+++ HealthApp.py
@@ -2,4 +2,5 @@ weight = int(input("Your weight: "))
 height = int(input("Your height: "))
 age = int(input("Your age: "))
 
-print("hello world")
+maintain = (f"To maintain your current weight, you need to consume {10*weight + 6.25*height - 5*age + 5} calories")
+print(maintain)
