bfc83e99ea328ca2d9b7a9f4a848c2b428267956
GegeeTs <tsgegee1030@gmail.com>
2023-10-19 19:16:38 -0600

    jjjj

--- HealthApp.py
+++ HealthApp.py
@@ -2,12 +2,14 @@ weight = int(input("Your weight: "))
 height = int(input("Your height: "))
 age = int(input("Your age: "))
 
-<<<<<<< HEAD
+
+
+
 formula = int(10 * weight + 6.25 * height - 5 * age + 5)
 calories = 400 + formula
 print("To gain weight, you need" ,  calories , "calories per day")
 
-=======
+
 minorDeficit = (10*weight) + (6.25*height) + (5*age) - 250
 majorDeficit = (10*weight) + (6.25*height) + (5*age) - 500
 
@@ -19,4 +21,3 @@ majorDeficit = (10*weight) + (6.25*height) + (5*age) - 500
 
 maintain = (f"To maintain your current weight, you need to consume {10*weight + 6.25*height - 5*age + 5} calories")
 print(maintain)
->>>>>>> ab94c131fa061366af61644c6d765b1fc575c3ef
