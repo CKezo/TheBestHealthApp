faef7620bcf532b494c7c5aa3598873a68ecef2a
GegeeTs <tsgegee1030@gmail.com>
2023-10-26 18:29:13 -0600

    jjj

--- HealthApp.py
+++ HealthApp.py
@@ -1,34 +1,33 @@
 weight = int(input("Your weight: "))
 height = int(input("Your height: "))
 age = int(input("Your age: "))
-gender = int(input("Are you female 1:, or male 2: "))
-choice = int(input("Minor 1: , or Major 2: "))
+gender = int(input("Are you male 1:, or female 2: "))
 numberChoice = int(input("Would you like to lose weight 1:, maintain weight 2:, or gain weight 3: "))
 
-formula_1 = 10 * weight + 6.25 * height - 5 * age + 5 #male calories
-minorgainCalories_1 = 250 + formula_1
-majorgainCalories_1 = 500 + formula_1
+formula_1 = 10 * weight + 6.25 * height - 5 * age + 5 #male calories #maintain
+minorgainCalories_1 = 250 + formula_1 #gaining minor
+majorgainCalories_1 = 500 + formula_1 #gaining major
 
-formula_2 = 10 * weight + 6.25 * height - 5 * age - 161 #female calories
-minorgainCalories_2 = 250 + formula_2
-majorgainCalories_2 = 500 + formula_2
+formula_2 = 10 * weight + 6.25 * height - 5 * age - 161 #female calories #maintain
+minorgainCalories_2 = 250 + formula_2 #gaining minor 
+majorgainCalories_2 = 500 + formula_2 #gaining major
 
 
-minorDeficit_1 = formula_1 - 250
-minorDeficit_1 = formula_1 -  500
+minorDeficit_1 = formula_1 - 250 #minordeficit male
+majorDeficit_1 = formula_1 - 500 #majordeficit male 
 
-majorDeficit_2 = formula_2 -  250
-majorDeficit_2 = formula_2 - 500
+minorDeficit_2 = formula_2 - 250 #minordeficit female
+majorDeficit_2 = formula_2 - 500 #majordeficit female
 
 
 
 if gender == 1:
     if numberChoice == 1:
-        print(f"In order to lose weight, you should eat {minorDeficit} calories per day")
-    elif numberChoice == 2:
-        print(f"In order to maintain weight, you should eat {formula_1} calories per day")
-    elif numberChoice == 3:
-        print(f"In order to gain weight, you should eat {gainCalories_1} calories per day")
+            print(f"In order to lose weight, you should eat {minorDeficit_1} calories per day")
+        elif numberChoice == 2:
+            print(f"In order to maintain weight, you should eat {formula_1} calories per day")
+        elif numberChoice == 3:
+            print(f"In order to gain weight, you should eat {gainCalories_1} calories per day")
 elif gender == 2:
     if numberChoice == 1:
         print(f"In order to lose weight, you should eat {minorDeficit} calories per day")
